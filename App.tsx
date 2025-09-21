2b0925c037f68f8286f1a1850efc972cff716fea
shuvajitmaitra <shuvajitmaitra@gmail.com>
2025-09-21 10:55:20 +0600

    tailwind setup and used yarn instead npm

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import "./global.css";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is Nitto Ponno Mobile App</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-blue-300">
+      <Text className="text-xl font-bold text-blue-500">Welcome to Nativewind!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
