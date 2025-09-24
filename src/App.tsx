2277b5f568302853d0a2426de9f51e7a88094ea9
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2025-09-24 23:19:04 +0600

    Confilict main/setup/npa 2 font family setup (#13)
    * Dev (#3)
    
    * tailwind setup and used yarn instead npm
    
    * issue fix
    
    ---------
    
    Co-authored-by: shuvajitmaitra <shuvajitmaitra@gmail.com>
    
    * Conflict test/dev (#7)
    
    * tailwind setup and used yarn instead npm
    
    * issue fix
    
    * nativewind issues fix. Edit in tailwind.config.js file
    
    ---------
    
    Co-authored-by: shuvajitmaitra <shuvajitmaitra@gmail.com>
    
    * Dev (#6)
    
    * tailwind setup and used yarn instead npm
    
    * issue fix
    
    * nativewind issues fix. Edit in tailwind.config.js file
    
    ---------
    
    Co-authored-by: shuvajitmaitra <shuvajitmaitra@gmail.com>
    
    * Refactor code structure for improved readability and maintainability NPA-2
    
    * feat: add NText component for consistent text styling across the application
    
    ---------
    
    Co-authored-by: farukulWD <121636449+farukulWD@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import "./global.css";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { useFonts } from "expo-font";
+import NText from "./components/global/NText";
+
+// Keep splash screen until we hide it manually
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Thin: require("./assets/fonts/Roboto-Thin.ttf"),
+    Light: require("./assets/fonts/Roboto-Light.ttf"),
+    Regular: require("./assets/fonts/Roboto-Regular.ttf"),
+    Medium: require("./assets/fonts/Roboto-Medium.ttf"),
+    SemiBold: require("./assets/fonts/Roboto-SemiBold.ttf"),
+    Bold: require("./assets/fonts/Roboto-Bold.ttf"),
+    ExtraBold: require("./assets/fonts/Roboto-ExtraBold.ttf"),
+  });
+  useEffect(() => {
+    const prepare = async () => {
+      // simulate loading
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      await SplashScreen.hideAsync();
+    };
+    prepare();
+  }, []);
+
   return (
     <View className="flex-1 items-center justify-center bg-blue-300">
-      <Text className="text-xl font-bold text-blue-500">Welcome to Nativewind!</Text>
+      <NText className="text-xl font-FFRegular  text-blue-500">Welcome to Nitto Ponno!</NText>
     </View>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./src/App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/App.tsx", "./components/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        FFThin: ["Thin"],
+        FFLight: ["Light"],
+        FFRegular: ["Regular"],
+        FFMedium: ["Medium"],
+        FFSemibold: ["SemiBold"],
+        FFBold: ["Bold"],
+        FFExtrabold: ["ExtraBold"],
+      },
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        background: "var(--color-background)",
+        foreground: "var(--color-foreground)",
+        heading: "var(--color-heading)",
+        body: "var(--color-body)",
+        line: "var(--color-line)",
+        border: "var(--color-border)",
+        success: "var(--color-success)",
+        warning: "var(--color-warning)",
+        error: "var(--color-error)",
+      },
+    },
   },
   plugins: [],
 };
