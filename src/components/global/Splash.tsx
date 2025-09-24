2e2c97236f173216b91ca4bd3343c1dc14c4d1ee
shuvajitmaitra <shuvajitmaitra@gmail.com>
2025-09-24 23:11:39 +0600

    ddd

--- src/components/global/Splash.tsx
+++ src/components/global/Splash.tsx
@@ -3,8 +3,8 @@ import { Image, View } from "react-native";
 
 const Splash = () => {
   return (
-    <View className="bg-white relative h-full w-full justify-center items-center ">
-      <Image source={require("../../assets/icon.jpg")} className="w-80 h-80 object-contain -mt-10" resizeMode="contain" />
+    <View className="bg-primary relative h-full w-full justify-center items-center ">
+      <Image source={require("../../assets/images/icon.jpg")} className="w-80 h-80 object-contain -mt-10" resizeMode="contain" />
     </View>
   );
 };
