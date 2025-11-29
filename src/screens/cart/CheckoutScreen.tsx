5dc5be4ecf5d914e160a65560254237844349843
shuvajitmaitra <shuvajitmaitra@gmail.com>
2025-11-30 01:39:04 +0600

    Refactor checkout screen to wrap address and selected items sections in a ScrollView

--- src/screens/cart/CheckoutScreen.tsx
+++ src/screens/cart/CheckoutScreen.tsx
@@ -4,13 +4,16 @@ import CheckoutHeader from "@/components/checkout/CheckoutHeader";
 
 import AddressSection from "@/components/checkout/AddressSection";
 import SelectedItemsSection from "@/components/checkout/SelectedItemsSection";
+import { ScrollView } from "react-native";
 
 const CheckoutScreen = () => {
   return (
     <SafeAreaView className="flex-1 bg-background px-4">
       <CheckoutHeader />
-      <AddressSection />
-      <SelectedItemsSection />
+      <ScrollView>
+        <AddressSection />
+        <SelectedItemsSection />
+      </ScrollView>
     </SafeAreaView>
   );
 };
